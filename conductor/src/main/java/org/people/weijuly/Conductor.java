8c387717d62a0db5c03cfdf68fc6c2c81a2fbce4
GopiKrishnan Ganesan <gopikrishnan.ganesan@gmail.com>
2019-01-10 10:43:35 +0530

    initial conductor draft

---
+++ conductor/src/main/java/org/people/weijuly/Conductor.java
@@ -0,0 +1,13 @@
+package org.people.weijuly;
+
+public class Conductor {
+
+	public static void main(String[] args) {
+
+		while (true) {
+			// get game status
+			// configurable delay
+			// post ball
+		}
+	}
+}
---
+++ conductor/src/test/java/org/people/weijuly/ConductorTest.java
@@ -0,0 +1,15 @@
+package org.people.weijuly;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class ConductorTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
